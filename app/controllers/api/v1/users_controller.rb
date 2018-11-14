4eb59d742cc5b14a78f27927ca20af88f0e6aefd
srscalia <shelby.scalia@gmail.com>
2018-11-14 08:48:57 -0500

    Add kitty photos for seed data

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,4 +10,14 @@ class Api::V1::UsersController < ApplicationController
     render json: @user, status: :ok
   end
 
+  def create
+    @user = User.create(user_params)
+    render json: @user, status: :created
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :wins, :losses)
+  end
 end
